377f8fa612fa41f515a31b2522fea06704ce4eb2
SonPT <>
2021-10-10 22:39:40 -0400

    deploy 2 - test wpa

---
+++ sw.js
@@ -0,0 +1,31 @@
+var cacheName = "cammelia-pwa";
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/contact.html',
+    '/painting.html',
+    '/photo.html',
+    '/css/style.css',
+    '/js/main.js',
+    '/images/'
+];
+
+// Start and cache all the needed files
+self.addEventListener('install', function(e){
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+//Serve cache when offline
+self.addEventListener('fetch', function(e){
+    e.respondWith(
+        caches.match(e.request).then(function(response){
+            return response || fetch(e.request);
+        })
+    );
+
+
+});
