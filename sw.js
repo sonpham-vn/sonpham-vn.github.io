291d9abbf7593c576dec059b3700456ce1d959e2
SonPT <>
2021-10-10 22:48:58 -0400

    test deploy

--- sw.js
+++ sw.js
@@ -1,31 +1,28 @@
 var cacheName = "cammelia-pwa";
 var filesToCache = [
-    '/',
-    '/index.html',
-    '/contact.html',
-    '/painting.html',
-    '/photo.html',
-    '/css/style.css',
-    '/js/main.js',
-    '/images/common/paint-bg.jpg'
+  "/",
+  "/index.html",
+  "/contact.html",
+  "/painting.html",
+  "/photo.html",
+  "/css/style.css",
+  "/js/main.js"
 ];
 
 // Start and cache all the needed files
-self.addEventListener('install', function(e){
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache){
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 //Serve cache when offline
-self.addEventListener('fetch', function(e){
-    e.respondWith(
-        caches.match(e.request).then(function(response){
-            return response || fetch(e.request);
-        })
-    );
-
-
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
